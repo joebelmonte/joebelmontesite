e5afa3c54c707edd7570d65b854e172fadd201ee
Joseph Belmonte <joseph.belmonte@gmail.com>
2019-10-30 10:53:16 -0400

    Dynamic link working on button click for transition

--- crossdomain.js
+++ crossdomain.js
@@ -1,18 +1,20 @@
 var redirect = function() {
-  console.log("redirecting");
-  window.location = "https://glancepro.online";
+  if (!GLANCE.Cobrowse.Visitor.inSession()) {
+    window.location = "https://glancepro.online";
+  }
+  if (GLANCE.Cobrowse.Visitor.inSession()) {
+    GLANCE.Cobrowse.Visitor.continueSessionAt({
+      destination: "glancepro.online",
+      oncomplete: function() {
+        window.location = "https://glancepro.online";
+      },
+      iewarningsuppressed: true
+    });
+  }
 };
 
 window.addEventListener("load", event => {
-  document.getElementById("redirect").addEventListener("click", redirect);
-});
-
-GLANCE.Cobrowse.Visitor.addEventListener("sessionstart", () => {
-  GLANCE.Cobrowse.Visitor.continueSessionAt({
-    destination: "glancepro.online",
-    oncomplete: function() {
-      console.log("session continued on glancepro.online");
-    },
-    iewarningsuppressed: true
-  });
+  document
+    .getElementById("redirect-button")
+    .addEventListener("click", redirect);
 });
