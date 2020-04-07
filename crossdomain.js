36b4bf78c94acf90aff98038ae09b8b81864d58a
Joseph Belmonte <joseph.belmonte@gmail.com>
2020-04-07 10:09:31 -0400

    Got cross domain (new versions) and PDF working

--- crossdomain.js
+++ crossdomain.js
@@ -1,18 +1,42 @@
-var redirect = function() {
-  console.log("redirecting");
-  window.location = "https://glancepro.online";
-};
+// var redirect = function() {
+//   console.log("redirecting");
+//   window.location = "https://glancepro.online";
+// };
+//
+// window.addEventListener("load", event => {
+//   document.getElementById("redirect").addEventListener("click", redirect);
+// });
+//
+// GLANCE.Cobrowse.Visitor.addEventListener("sessionstart", () => {
+//   GLANCE.Cobrowse.Visitor.continueSessionAt({
+//     destination: "glancepro.online",
+//     oncomplete: function() {
+//       console.log("session continued on glancepro.online");
+//     },
+//     iewarningsuppressed: true
+//   });
+// });
+
+function redirectCrossDomain() {
+  console.log("in redirectCrossDomain");
+  var params = { url: "https://glancepro.online/", target: "_self" };
+  GLANCE.Cobrowse.Visitor.crossDomain(params);
+}
+
+function redirectPromptCrossDomain() {
+  console.log("in redirectPromptCrossDomain");
+  var params = { url: "https://glancepro.online/", target: "_self" };
+  GLANCE.Cobrowse.VisitorUI.promptCrossDomain(params);
+}
 
 window.addEventListener("load", event => {
-  document.getElementById("redirect").addEventListener("click", redirect);
+  document
+    .getElementById("redirectCrossDomain")
+    .addEventListener("click", redirectCrossDomain);
 });
 
-GLANCE.Cobrowse.Visitor.addEventListener("sessionstart", () => {
-  GLANCE.Cobrowse.Visitor.continueSessionAt({
-    destination: "glancepro.online",
-    oncomplete: function() {
-      console.log("session continued on glancepro.online");
-    },
-    iewarningsuppressed: true
-  });
+window.addEventListener("load", event => {
+  document
+    .getElementById("redirectPromptCrossDomain")
+    .addEventListener("click", redirectPromptCrossDomain);
 });
